b3aa87e6df639a0589d52cdce2f7891c793686ab
Anselmo-cpu <anselmoui16@gmail.com>
2026-01-09 16:32:30 -0300

    TEST RESPONSIVE faviicon etc

--- JS/main.js
+++ JS/main.js
@@ -24,6 +24,14 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+document.addEventListener("DOMContentLoaded", function() {
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+
+  hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+});
 
 
 
